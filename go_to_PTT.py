4a598ec79153a1f684f9f97de4edb6c1525f8b70
Each Chen <iach526526@gmail.com>
2022-05-28 18:27:08 +0800

    Update go_to_PTT.py
    新增內容:
    1.讀到的結果書輸出到excle
    
    2.如果那頁有文章被刪掉會跳過那一頁，被刪掉的文章屍體會影響到後面的順序，不好處理，先迴避

--- go_to_PTT.py
+++ go_to_PTT.py
@@ -11,17 +11,57 @@ path="D:/chromedriver_win32/chromedriver.exe"
 driver=webdriver.Chrome(path)
 
 driver.get("https://www.ptt.cc/bbs/HatePolitics/index.html")
-cookie ={"name": "over18",'value':'1'}#更動cookie
+cookie ={"name": "over18",'value':'1'}
 driver.add_cookie(cookie)
 driver.get_cookies()
 driver.get("https://www.ptt.cc/bbs/HatePolitics/index.html")
-
-for j in range(10):#跑幾頁的PTT
+a=[]#迴圈內儲存作者、日期等要爬取的資料，等出迴圈匯入excle
+d=[]
+t=[]
+for j in range(10):#次數自己訂，會決定讀取的頁數
+    br=0
     WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.CLASS_NAME, "author"))
             )
     author=driver.find_elements_by_class_name('author')
+    
+    date=driver.find_elements_by_class_name("date")
+    
+    title=driver.find_elements_by_css_selector('div.title > a')
     for i in author:
-        print(i.text)
+        if (i.text)=="-":
+            br=1
+    if br==1:
+        next=driver.find_element_by_link_text('‹ 上頁')
+        next.click()
+        continue#我不知道怎麼處理被刪掉文章的屍體...屍體只會留著日期和被"-"取代的作者名，不好處理，直接捨棄整頁
+
+    for i in author:
+        a.append(i.text)
+    
+    for k in date:
+        d.append(k.text)
+    for q in title:
+        t.append(q.text)
     next=driver.find_element_by_link_text('‹ 上頁')
     next.click()
+
+#Excle的部分
+from openpyxl import Workbook,load_workbook
+wb=Workbook()
+ws=wb.active
+
+title=["日期","ID","標題"]
+
+ws.append(title)
+ws.merge_cells('C1:F1')
+
+
+
+for data in range(len(a)):
+    
+    print(a[int(data)]+"    "+d[int(data)]+t[int(data)])
+    temp=[d[int(data)],a[int(data)],t[int(data)]]
+    ws.append(temp)
+wb.save("爬蟲測試2.xlsx")#儲存檔案
+driver.quit()
